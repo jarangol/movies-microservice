4dc22a21a4e68b3f824606e774789c59c5aec0b3
jarangol <jarangolp@gmail.com>
2023-03-31 01:24:32 -0500

    Added cors exeption and added list all movies ep

--- src/main/java/co/edu/eafit/moviesapp/MoviesController.java
+++ src/main/java/co/edu/eafit/moviesapp/MoviesController.java
@@ -3,8 +3,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MoviesController {
 
@@ -13,8 +14,13 @@ public class MoviesController {
   public MoviesController(MoviesService moviesService) {
     this.moviesService = moviesService;
   }
-  @RequestMapping("/{movieId}")
+  @RequestMapping("/movies/{movieId}")
   public @ResponseBody String getMovie(@PathVariable("movieId") String movieId) {
     return moviesService.getMovie(movieId);
+  }
+
+  @RequestMapping("/movies")
+  public @ResponseBody String getMovies() {
+    return moviesService.getMovies();
   }
 }
--- src/main/java/co/edu/eafit/moviesapp/MoviesService.java
+++ src/main/java/co/edu/eafit/moviesapp/MoviesService.java
@@ -5,18 +5,25 @@ import org.springframework.web.client.RestTemplate;
 
 public class MoviesService {
   private final RestTemplate restTemplate;
-
+  private final String host;
 
   public MoviesService() {
     this.restTemplate = new RestTemplate();
+    this.host = "http://159.122.183.100:32341/api/v1.0";
   }
 
   public MoviesService(RestTemplateBuilder restTemplateBuilder) {
     this.restTemplate = restTemplateBuilder.build();
+    this.host = "http://159.122.183.100:32341/api/v1.0";
   };
 
   public String getMovie(String movieId) {
-    String url = "http://159.122.183.100:32341/api/v1.0/films/" + movieId;
+    String url = this.host + "/films/" + movieId;
+    return this.restTemplate.getForObject(url, String.class);
+  }
+
+  public String getMovies() {
+    String url = this.host + "/films/";
     return this.restTemplate.getForObject(url, String.class);
   }
 }
